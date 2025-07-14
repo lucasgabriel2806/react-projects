8b252206d5aff07c7e778e0e79a036188af27e3b
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-14 14:54:49 -0300

    Projeto Random Color

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,17 @@ import Accordian from './components/accordian/Accordian';
 
 import Accordian2 from './components/accordian/Accordian2';
 
+import RandomColor from './components/random-color';
+
 function App() {
   return (
     <div className="App">
       
         {/* <Accordian /> */}
 
-        <Accordian2 />
+        {/* <Accordian2 /> */}
+
+        <RandomColor />
 
     </div>
   );
