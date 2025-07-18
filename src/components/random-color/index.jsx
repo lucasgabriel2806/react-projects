09cc4bb2e2c3cdfcefaba48fc255a77c79368e15
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-18 16:01:41 -0300

    Projeto Random Color #3

--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -8,27 +8,44 @@ export default function RandomColor() {
     // state para definir a cor
     const [color, setColor] = useState('#000000');
 
+    // Função para gerar um número aleatório
     function randomColorUtility(length) {
 
+        // Retorna um número aleatório de 0 a 16
         return Math.floor(Math.random() * length);
 
     }
 
-    function handleCreateRandomRexColor() {
+    function handleCreateRandomHexColor() {
 
         const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 
         let hexColor = '#';
 
-        for (let i; i < 6; i++) {
+        // Executa 6 vezes pra preencher o valor hexadecimal.
+        for (let i = 0; i < 6; i++) {
 
+            // Preenchendo o valor hexadecimal
             hexColor += hex[randomColorUtility(hex.length)];
 
         }
 
+        console.log(hexColor);
+
+        // atualizando o state color para ter o valor de hexColor
+        setColor(hexColor);
+
     }
 
     function handleCreateRandomRgbColor() {
+
+        const r = randomColorUtility(256);
+
+        const g = randomColorUtility(256);
+
+        const b = randomColorUtility(256);
+
+        setColor(`rgb${r}, ${g}, ${b}`);
 
     }
 
@@ -46,12 +63,10 @@ export default function RandomColor() {
             {/* Coverter para rgb */}
             <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
 
-            {console.log(typeOfColor)}
-
             {/* Gerar uma cor aleatória */}
             <button 
                 onClick={
-                    typeOfColor === 'hex' ? handleCreateRandomRexColor : handleCreateRandomRgbColor
+                    typeOfColor === 'hex' ? handleCreateRandomHexColor : handleCreateRandomRgbColor
                     }
                 >
                 Generate Random Color
