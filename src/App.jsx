7b5d8000f2b2c9f2408906e7e642226090bea3fa
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-18 08:56:49 -0300

    Projeto Random Color #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import './App.css';
+
 import Accordian from './components/accordian/Accordian';
 
 import Accordian2 from './components/accordian/Accordian2';
--- src/components/random-color/index.jsx
+++ src/components/random-color/index.jsx
@@ -1,11 +1,36 @@
 import { useState } from "react"
 
-
 export default function RandomColor() {
 
-const [typeOfColor, setTypeOfColor] = useState('hex');
+    // state para definir o tipo da cor (hex ou rgb)
+    const [typeOfColor, setTypeOfColor] = useState('hex');
 
-const [color, setColor] = useState('#000000');
+    // state para definir a cor
+    const [color, setColor] = useState('#000000');
+
+    function randomColorUtility(length) {
+
+        return Math.floor(Math.random() * length);
+
+    }
+
+    function handleCreateRandomRexColor() {
+
+        const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+        let hexColor = '#';
+
+        for (let i; i < 6; i++) {
+
+            hexColor += hex[randomColorUtility(hex.length)];
+
+        }
+
+    }
+
+    function handleCreateRandomRgbColor() {
+
+    }
 
     return (
 
@@ -15,11 +40,22 @@ const [color, setColor] = useState('#000000');
             background: color,
         }}>
 
-            <button>Create HEX Color</button>
+            {/* Coverter para hex */}
+            <button onClick={() => setTypeOfColor('hex')}>Create HEX Color</button>
 
-            <button>Create RGB Color</button>
+            {/* Coverter para rgb */}
+            <button onClick={() => setTypeOfColor('rgb')}>Create RGB Color</button>
 
-            <button>Generate Random Color</button>
+            {console.log(typeOfColor)}
+
+            {/* Gerar uma cor aleatória */}
+            <button 
+                onClick={
+                    typeOfColor === 'hex' ? handleCreateRandomRexColor : handleCreateRandomRgbColor
+                    }
+                >
+                Generate Random Color
+            </button>
 
         </div>
 
