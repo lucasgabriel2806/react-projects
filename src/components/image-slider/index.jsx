073d12d3bdbfa95c7b1aa0c6d7a2f50e168a3f4b
Lucas Gabriel de Paula Pinto <lucas.gabriellgpp@gmail.com>
2025-07-25 15:44:57 -0300

    Image Slider #02

--- src/components/image-slider/index.jsx
+++ src/components/image-slider/index.jsx
@@ -6,9 +6,40 @@ export default function ImageSlider(url, limit) {
 
     const [currentSlide, setCurrentSlide] = useState(0);
 
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    const [loading, setLoading] = useState(false);
+
     async function fetchImages(getUrl) {
 
+        try {
 
+            // Começa a carregar e chama a API
+            setLoading(true);
+
+            const response = await fetch(getUrl);
+
+            const data = await response.json();
+
+            // Se existir dados
+            if (data) {
+
+                // atualizaremos nossa imagem com esses dados 
+                setImages();
+
+                // e paramos de carrregar
+                setLoading(false);
+
+            }
+
+        } catch(e) {
+
+            setErrorMsg(e.message);
+
+            // Se deu erro (catch) para de carregar
+            setLoading(false);
+
+        }
 
     }
 
@@ -16,6 +47,14 @@ export default function ImageSlider(url, limit) {
         // Se existir uma url, busque a imagem
         if(url !== '') fetchImages(url);
     }, [url]);
+
+    if(loading) {
+        return <div>Loading data ! Please wait</div>
+    }
+
+    if (errorMsg !== null) {
+        return <div>Error occured ! {errorMsg}</div>
+    }
 
     return <div className="container"></div>
 
